ff253510e33ee3429482c403d9c2cec370ef7917
Adrian Barreno <adrian.barreno@alumnos.upm.es>
2022-10-29 14:54:15 +0200

    Gene class

--- creating_objects/test_script.rb
+++ creating_objects/test_script.rb
@@ -1,6 +1,8 @@
 
 ## We create and object File named seed_stock.
 
-seed_stock = File.open("./files/seed_stock_data.tsv", "r")
-seed_stock.each.next
-data = Array.new.(puts seed_stock.each.next)
+gene_file = File.open("./files/gene_information.tsv", "r")
+
+puts gene_file.methods - Object.methods
+
+
