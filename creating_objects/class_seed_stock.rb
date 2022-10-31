74a944312e054ae88584d6d9919427e6e6011a6b
PabloM <pyblus@gmail.com>
2022-10-31 17:45:47 +0100

    updated class for bonus score

--- creating_objects/class_seed_stock.rb
+++ creating_objects/class_seed_stock.rb
@@ -1,43 +1,54 @@
-class Seedstock
+class Stockdatabase
   require 'csv'
   attr_accessor :table
-  attr_accessor :stockID
-  attr_accessor :mutant
+  attr_accessor :headers
   attr_accessor :path
+  attr_accessor :stockpath
   
   def initialize (stockpath:)
     unless File.file?(stockpath)
       return ("File doesn't exist")
     else
-      @path = Dir.pwd
-      @stockpath = stockpath
-      @stockID = stockID
-      @mutant = mutant
-      @table = CSV.read(File.open(stockpath), headers: true, col_sep: "\t")
+      @path = Dir.pwd # Current working directory
+      @stockpath = stockpath # Seed_stock_data.tsv path
+      @table = CSV.read(File.open(stockpath), headers: true, col_sep: "\t") # Importing stockfile as table
+      @headers = table.headers
     end
   end
 
 
-  def seeding (seednum)
+
+  def new_database (database)
     path = @path
     newstock = @table
     newstock.each { |row| #Iterating over each row
-      row[4] = (row[4].to_i - seednum) # Substraction of input from Grams_Remaining
+      row[4] = (row[4].to_i - 7) # Substraction of input from Grams_Remaining
       if row[4] <= 0 # return error message if value < 0
         row[4]=0
         $stderr.puts "WARNING: we have run out of Seed Stock #{row[0]}"
       end
     }
-    CSV.open("#{path}/newstock.tsv", 'w', col_sep: "\t") do |tsv|
+    CSV.open("#{path}/#{database}", 'w', col_sep: "\t") do |tsv| #Creating file to save new data in pwd
       tsv << newstock.headers
       newstock.each { |row| tsv << row }
     end
     return newstock
   end
   
-  def linking (stockid, mut_gene)
-    @stockID = stockid
-    @mutant = mut_gene
+  ["Seed_Stock", "Mutant_Gene_ID", "Last_Planted", "Storage", "Grams_Remaining"].each do |header|
+    define_method "get_#{header}" do |match|
+        col = @table[header]
+        index = col.each_index.select{|x| col[x] == match}
+        puts @headers.join(',')
+        index.each { |i| puts "#{@table[i]}" }
+    end
   end
     
 end
+
+#Testing
+#stock = Stockdatabase.new(stockpath: "/home/osboxes/bioinfogit/bioinfo_Chals/seed_stock_data.tsv")
+#stock.get_Storage('cama25')
+#puts stock.table
+#puts stock.new_database('newstock.tsv')
+#stock.headers
