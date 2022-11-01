ec39cab476ba2281794085ffd1bcb67c0c25e57d
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-01 22:59:42 +0100

    Creating proccessing script and loading neccesary packages (classes)

---
+++ creating_objects/Proccessing.rb
@@ -0,0 +1,13 @@
+
+# Importing required classes ("path to classes")
+require './class_cross_data.rb'
+require './class_seed_stock.rb'
+require './class_genes.rb'
+
+# https://stackoverflow.com/questions/28488422/how-to-check-the-number-of-arguments-passed-with-a-ruby-script 
+# How to access to arguments in ruby
+
+
+
+# Accessing to grams remaind of each kind of seed and simulating planting 7 grams
+
