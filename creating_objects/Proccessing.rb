108befb662c9f7b16ad627bb97f5df869d9bc8a5
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-02 15:51:39 +0100

    Updating

--- creating_objects/Proccessing.rb
+++ creating_objects/Proccessing.rb
@@ -7,7 +7,22 @@ require './class_genes.rb'
 # https://stackoverflow.com/questions/28488422/how-to-check-the-number-of-arguments-passed-with-a-ruby-script 
 # How to access to arguments in ruby
 
+#Checking if the arguments required are specified 
+if ARGV.length != 4 then # Paths to classes are required as arguments?
+    abort("Files pathways are required")
+end
+
+seed_stock_file=ARGV[0]
+gene_file=ARGV[1]
+cross_data_file=ARGV[2]
+output=ARGV[3]
 
 
 # Accessing to grams remaind of each kind of seed and simulating planting 7 grams
+puts 'Planting 7 g of seeds'
+puts '...'
+puts '...'
+puts "#{stock}.new_database(#{ARGV[3]})" # The output is a table where the grams of seed remains and the last date of plant is contained
+puts ''
+puts ''
 
