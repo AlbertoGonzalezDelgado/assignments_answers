0bd09db352e225beeec44f087e71eee8cd22a87a
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-08 15:42:46 +0100

    Debuging

--- creating_objects/adri_class/class/class_seed_stock.rb
+++ creating_objects/adri_class/class/class_seed_stock.rb
@@ -1,13 +1,14 @@
+#Importing required library
 require 'csv'
 
 class SeedStock
-  
+  #Defining atribute accessors
   attr_accessor :seed_stock
   attr_accessor :mutant_gene_id
   attr_accessor :last_planted
   attr_accessor :storage
   attr_accessor :grams_remaining
-  
+  #Defining .new method with its properties
   def initialize(seed_stock:, mutant_gene_id:, last_planted:, storage:, grams_remaining:)
       @seed_stock = seed_stock
       @mutant_gene_id = mutant_gene_id
