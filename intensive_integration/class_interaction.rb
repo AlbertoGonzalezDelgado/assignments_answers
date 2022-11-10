cf488473181592729be59914b0fbe49a8e6b1b65
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-10 16:03:23 +0100

    Updating

---
+++ intensive_integration/class_interaction.rb
@@ -0,0 +1,18 @@
+#Creating an Interaction Network Object to contain the members of each network
+require 'rest-client'
+class InteractionNetwork
+    atrr_accesor :gene_id
+    atrr_accesor :relations
+
+    def initialize(gene_id:,relations:)
+        @gene_id = :gene_id
+        @relations = :relations 
+    end
+
+    def find_interactions(gene_id:)
+        Interactions = Array.ne #Creating an empty array to save interactions
+        #https://code-maven.com/how-to-convert-a-string-to-uppercase-or-lowercase-in-ruby
+        gene_id=gene_id.upcase #When comparing I find more usefull upcase or low case the name because the format may be different
+end
+
+#InteractionNetwork.acces_info(https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id= , At3g54340)
