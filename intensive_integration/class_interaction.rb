4c507f1f6252ecd9775663fb9d3d217af77cd540
Adrian Barreno <adrian.barreno@alumnos.upm.es>
2022-11-10 18:46:34 +0100

    socorro

--- intensive_integration/class_interaction.rb
+++ intensive_integration/class_interaction.rb
@@ -10,9 +10,10 @@ class InteractionNetwork
     end
 
     def find_interactions(gene_id:)
-        Interactions = Array.ne #Creating an empty array to save interactions
+        Interactions = Array.new #Creating an empty array to save interactions
         #https://code-maven.com/how-to-convert-a-string-to-uppercase-or-lowercase-in-ruby
         gene_id=gene_id.upcase #When comparing I find more usefull upcase or low case the name because the format may be different
+    end
 end
 
 #InteractionNetwork.acces_info(https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id= , At3g54340)
---
+++ intensive_integration/main.rb
@@ -0,0 +1,12 @@
+'''
+No entiendo una mierda :(
+
+PSICQUIC:
+http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/At4g18960?format=tab25 
+
+DBFetch:
+https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=At3g54340
+
+'''
+
+
