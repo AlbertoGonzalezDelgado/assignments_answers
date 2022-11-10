a306610f879ed132f820f6a3421fd2c8bedfedd8
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-10 15:52:20 +0100

    Uploading

--- intensive_integration/main.rb
+++ intensive_integration/main.rb
@@ -1,4 +1,5 @@
 #Creating an Interaction Network Object to contain the members of each network
+require 'rest-client'
 class InteractionNetwork
     atrr_accesor :gene_id
     atrr_accesor :relations
@@ -7,4 +8,10 @@ class InteractionNetwork
         @gene_id = :gene_id
         @relations = :relations 
     end
+
+    def access_info(url_base: ,gene_id:)
+        adress=puts "#{url_base}#{gene_id}"
 end
+
+#InteractionNetwork.acces_info(https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id= , At3g54340)
+
