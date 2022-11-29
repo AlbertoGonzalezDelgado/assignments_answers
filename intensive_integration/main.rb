6df3273eaa4a3500c2cede5f4216ec73454ddc1c
AlbertoGonzalezDelgado <albertogonzdelgado@gmail.com>
2022-11-29 13:09:30 +0100

    Inputs checking

--- intensive_integration/main.rb
+++ intensive_integration/main.rb
@@ -1,17 +1,24 @@
-'''
-No entiendo una mierda :(
+#Checking if the arguments required are specified 
+unless ARGV.length == 2
+    abort("FATAL ERROR: Files pathways are required. \nHELP MESSAGE: Check README.md for more information.")
+end
 
-PSICQUIC:
-http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/At4g18960?format=tab25 
+#Checking if the input file exists
 
-DBFetch:
-https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=At3g54340
+unless File.file?(ARGV[0])
+    abort("FATAL ERROR: File #{ARGV[0]} does not exist or the pathway specified is not correct")
+end
 
-'''
 
-require './class_gene_characteristics.rb'
-
-puts gene_character.search_go("AT2G21330")
-ss
-
+#Checking if the output file already exists and asking if it should be overwrite
+if File.file?(ARGV[1])
+    puts "#{ARGV[1]} already exists, indicate if you want to overwrite [Y/N]" 
+    stdin = ""
+    until stdin == "n" || stdin == "N" || stdin == "y" || stdin == "Y"
+        stdin = STDIN.gets.strip
+        if stdin == "N" || stdin == "n"
+            abort("Run cancelled")
+        end
+    end
+end
 
