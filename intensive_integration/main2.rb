d121b7496621458298c0a1bcab8bb6205c17d2f5
Julian Politsch <jepolitsch@gmail.com>
2022-12-02 13:50:16 +0100

    fix

--- intensive_integration/main2.rb
+++ intensive_integration/main2.rb
@@ -81,8 +81,8 @@ gene_file = CSV.read(gene_information, col_sep: "\t")
 s2 = fetch(url: "https://doi.org/10.1371/journal.pone.0108567.s001")
 gene_list = []
 gene_file.each do |line|
-  unless line.match(/AT\dG\d{5}/i)
-    abort("ERROR: the gene list have some errors. #{line} has not correct format")
+  unless line[0].match(/AT\dG\d{5}/i)
+    abort("ERROR: the gene list have some errors. #{line[0} has not correct format")
   else
   gene_list.append([line[0].upcase])
   end
@@ -94,21 +94,3 @@ InteractionNetwork.new(depth=3, gene_list=gene_list)
 #puts InteractionNetwork.full_interactions
 #puts InteractionNetwork.full_network
 #puts InteractionNetwork.multi_gene_list
-
-=begin
-response = RestClient::Request.execute(  #  or you can use the 'fetch' function we created last class
-  method: :get,
-  url: "https://doi.org/10.1371/journal.pone.0108567",
-  headers: {accept: 'text/plain'}
-#  headers: {accept: 'application/json'}
-#  headers: {accept: 'text/turtle'}
-)  
-puts response.body.force_encoding('UTF-8')
-=end
-#puts s2.body.force_encoding('UTF-8')
-#end
-
-
-base_url = "www.ebi.ac.uk/intact/ws/interaction/findInteractions/{query}"
-#puts gene_info
-
