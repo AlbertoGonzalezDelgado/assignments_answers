3dac786bdf692776e982d268b853eb7f6077b144
Alberto González Delgado <93140174+AlbertoGonzalezDelgado@users.noreply.github.com>
2022-12-04 23:09:08 +0100

    Add files via upload

---
+++ intensive_integration/fetch.rb
@@ -0,0 +1,27 @@
+require 'rest-client'
+
+
+# Create a function called "fetch" that we can re-use everywhere in our code
+
+def fetch(url:, headers: {accept: "*/*"}, user: "", pass: "")
+  response = RestClient::Request.execute({
+    method: :get,
+    url: url.to_s,
+    user: user,
+    password: pass,
+    headers: headers})
+  return response
+  
+  rescue RestClient::ExceptionWithResponse => e
+    $stderr.puts e.inspect
+    response = false
+    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  rescue RestClient::Exception => e
+    $stderr.puts e.inspect
+    response = false
+    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+  rescue Exception => e
+    $stderr.puts e.inspect
+    response = false
+    return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
+end 
