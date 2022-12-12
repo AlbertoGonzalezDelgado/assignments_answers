76cfa698301f77800e0b84972615ee443c05afed
Alberto González Delgado <93140174+AlbertoGonzalezDelgado@users.noreply.github.com>
2022-12-12 16:35:37 +0100

    Adding input checking

--- orthologues/main.rb
+++ orthologues/main.rb
@@ -1,16 +1,64 @@
 require 'bio'
 
+#Checking the number of inputs
+unless ARGV.length == 2
+    abort("FATAL ERROR: Files pathways are required. \nHELP MESSAGE: Check README.md for more information.")
+end
+
+#Checking if the files pathways are well specified
+ARGV[0..1].each do |arg|
+    unless File.file?(arg)
+       abort("FATAL ERROR: File #{arg} does not exist or the pathway provided is not correct.")
+    end
+end
+
+#Checking if the files pathways are not the same
+if ARGV[0] == ARGV[1]
+    abort("FATAL ERROR: You have provided the same pathway twice!")
+end
+
+#Checking if the files specified have fasta format
+ARGV[0..1].each do |arg|
+    if arg.split('.')[-1]!='fasta' && arg.split('.')[-1]!='fa'  && arg.split('.')[-1]!='ffn' && arg.split('.')[-1]!='fna' && arg.split('.')[-1]!='faa' && arg.split('.')[-1]!='frn'    
+       abort("FATAL ERROR: File #{arg} has not fasta format")
+    end
+end
+
+puts 'The files are being imported sucesfully'
+puts ''
+puts ''
+sleep 1
+puts "Searching for orthologues between #{ARGV[0]} and #{ARGV[1]}"
+sleep 1
+#Source: https://www.asciiart.eu/computers/computers
+puts ' _______________                        |*\_/*|________'
+puts '|  ___________  |     .-.     .-.      ||_/-\_|______  |'
+puts '| |           | |    .****. .****.     | |           | |'
+puts '| |   0   0   | |    .*****.*****.     | |   0   0   | |'
+puts '| |     -     | |     .*********.      | |     -     | |'
+puts '| |   \___/   | |      .*******.       | |   \___/   | |'
+puts '| |___     ___| |       .*****.        | |___________| |'
+puts '|_____|\_/|_____|        .***.         |_______________|'
+puts '  _|__|/ \|_|_.............*.............._|________|_'
+puts ' / ********** \                          / ********** \ '
+puts '/  ************\                        /  ************\ '
+puts '------------------                      -----------------'
+
+sleep 1
+
+################################# CHECK FROM HERE ##########################################
+
 # Set the database paths for Arabidopsis and S. pombe
-arabidopsis_db_path = '/path/to/arabidopsis/database'
-spombe_db_path = '/path/to/s.pombe/database'
+arabidopsis_db_path = ARGV[0]
+spombe_db_path = ARGV[1]
 
 # Create a BLAST factory for each species
 arabidopsis_factory = Bio::Blast.local('blastn', arabidopsis_db_path)
 spombe_factory = Bio::Blast.local('blastn', spombe_db_path)
 
 # Read the Arabidopsis and S. pombe sequences from fasta files
-arabidopsis_fasta = Bio::FastaFormat.open('arabidopsis.fasta')
-spombe_fasta = Bio::FastaFormat.open('s.pombe.fasta')
+arabidopsis_fasta = Bio::FastaFormat.open('arabidopsis.fa')
+spombe_fasta = Bio::FastaFormat.open('s_pombe.fa')
 
 # Create a hash to store the reciprocal best hits
 reciprocal_best_hits = {}
