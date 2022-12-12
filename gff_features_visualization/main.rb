400ec184a64e7310e6dfbe77bec1d6ad2d084f2d
Alberto González Delgado <93140174+AlbertoGonzalezDelgado@users.noreply.github.com>
2022-12-12 16:38:07 +0100

    Adding inputs checking

--- gff_features_visualization/main.rb
+++ gff_features_visualization/main.rb
@@ -2,11 +2,12 @@ require './data_base.rb'
 require 'csv'
 require 'bio'
 
-#Checking if the input file exists
-unless File.file?(ARGV[0])
-    abort("FATAL ERROR: File #{ARGV[0]} does not exist or the pathway specified is not correct")
-end
+#Checking the number of inputs
+#unless ARGV.length == CHANGE FROM THE NUMBER WE NEED
+#    abort("FATAL ERROR: Files pathways are required. \nHELP MESSAGE: Check README.md for more information.")
+#end
 
+#Importing file
 gene_file_path = ARGV[0]
 
 gene_list=Data_base.get_genelist(file_path: gene_file_path)
