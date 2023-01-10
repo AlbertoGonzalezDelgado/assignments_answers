5daaab7282883b399b6ba1daa41d609d4cb6ed95
Adrian Barreno <adrian.barreno@alumnos.upm.es>
2023-01-10 17:04:37 +0100

    First blast report

--- orthologues/main.rb
+++ orthologues/main.rb
@@ -1,35 +1,36 @@
 require 'bio'
-'''
+
 #Checking the number of inputs
-unless ARGV.length == 2
-    abort("FATAL ERROR: Files pathways are required. \nHELP MESSAGE: Check README.md for more information.")
+unless ARGV.length == 4
+    abort("FATAL ERROR: BLAST databases and FASTA files paths are required.
+      \nHELP MESSAGE: Check README.md for more information.")
 end
 
 #Checking if the files pathways are well specified
 ARGV[0..1].each do |arg|
-    unless File.file?(arg)
-       abort("FATAL ERROR: File #{arg} does not exist or the pathway provided is not correct.")
-    end
+  unless File.file?(arg+".phr")
+    abort("FATAL ERROR: Database #{arg} does not exist or the pathway provided is not correct.")
+  end
 end
-
-#Checking if the files pathways are not the same
-if ARGV[0] == ARGV[1]
-    abort("FATAL ERROR: You have provided the same pathway twice!")
+ARGV[2..3].each do |arg|
+  unless File.file?(arg)
+    abort("FATAL ERROR: Fasta file #{arg} does not exist or the pathway provided is not correct.")
+  end
 end
 
 #Checking if the files specified have fasta format
 #Source: https://es.wikipedia.org/wiki/Formato_FASTA
-ARGV[0..1].each do |arg|'''
-#    if arg.split('.')[-1]!='fasta' && arg.split('.')[-1]!='fa'  && arg.split('.')[-1]!='ffn' && arg.split('.')[-1]!='fna' && arg.split('.')[-1]!='faa' && arg.split('.')[-1]!='frn'    
-#       abort("FATAL ERROR: File #{arg} has not fasta format")
-#    end
-#end
+ARGV[2..3].each do |arg|
+  if arg.split('.')[-1]!='fasta' && arg.split('.')[-1]!='fa'  && arg.split('.')[-1]!='ffn' && arg.split('.')[-1]!='fna' && arg.split('.')[-1]!='faa' && arg.split('.')[-1]!='frn'    
+     abort("FATAL ERROR: File #{arg} is not Fasta format")
+  end
+end
 
 puts ''
 puts 'The files are being imported sucesfully'
 puts ''
 sleep 0.5
-puts "Searching for orthologues between Arabidopsis and S. pombe"
+puts "BLASTing Arabidopsis sequences on S. pombe proteome..."
 puts ''
 puts ''
 
@@ -42,20 +43,18 @@ spombe_db_path =  ARGV[1]
 # Create a BLAST factory for each species
 #system("makeblastdb -in files/TAIR10_cds_20101214_updated.fa -dbtype 'prot' -out databases/TAIR 2> /dev/null")
 #system("makeblastdb -in files/pep.fa -dbtype 'prot' -out databases/spombe 2> /dev/null")
-
-arabidopsis_factory = Bio::Blast.local('blastp', arabidopsis_db_path, "-F ‘m S’ -s T")
-spombe_factory = Bio::Blast.local('blastp', spombe_db_path, "-F ‘m S’ -s T")
+arabidopsis_factory = Bio::Blast.local('tblastn', arabidopsis_db_path, "-F T")  # protein query nucleic database
+spombe_factory = Bio::Blast.local('blastx', spombe_db_path, "-F T")             # nucleic query protein database
 
 # Read the Arabidopsis and S. pombe sequences from fasta files
 arabidopsis_fasta = Bio::FlatFile.auto(ARGV[2])
 spombe_fasta = Bio::FlatFile.auto(ARGV[3])
 
-# Create a hash to store the reciprocal best hits
-reciprocal_best_hits = {}
-
 
 ### ------------------ FIRST BLAST ------------------- ###
-# Create a file to save the results of Arabidopsis BLAST on S. pombe genome.
+
+# Create a file to save the unfiltered results of Arabidopsis BLAST on S. pombe genome. This
+# way we can adjust data filtering a posteriori without needing to repeat the Blast.
 first_blast = File.new("files/first_blast_unfiltered.txt", "w")
 first_blast.write("query\ttarget\te-value\tidentity\toverlap\tquery_length\tbit-score\tquery_seq\ttarget_seq\n")
 
@@ -66,7 +65,6 @@ arabidopsis_fasta.each_entry do |arabidopsis_seq|
   arabidopsis_results = spombe_factory.query(arabidopsis_seq)
 
   # Get the best hit from the BLAST results
-
   unless arabidopsis_results.hits.empty?
     
     # Print hits
@@ -82,12 +80,13 @@ arabidopsis_fasta.each_entry do |arabidopsis_seq|
   
     # Save hits in file
     first_blast.write("#{arabidopsis_seq.entry_id}\t#{hit.definition.split("|")[0]}\t#{hit.evalue}\t#{hit.identity}\t#{hit.overlap}\t#{hit.query_len}\t#{hit.bit_score}\t#{hit.query_seq}\t#{hit.target_seq}\n")
+  
   end
 end
 
 first_blast.close()
 
-## We can now read the results of the first Blast
+# We now retrieve the results of the first Blast into a new Hash
 first_hits = Hash.new
 
 File.readlines("files/first_blast_unfiltered.txt", chomp:true).each{ |hit|
@@ -96,20 +95,24 @@ File.readlines("files/first_blast_unfiltered.txt", chomp:true).each{ |hit|
   end
 }
 
+puts "BLAST of Arabidopsis sequences on S.pombe proteome is finished with a total number of #{first_hits.length} hits"
+puts  
+sleep 1
+puts "Finding reciprocal best hits in Arabidopsis proteome..."
+
 
 ### ----------------- SECOND BLAST ------------------ ###
 
-# Create a file to save the resutls of the reciprocal blast.
+# Create a file to save the unfiltered results of the reciprocal BLAST of S. pombe sequences on
+# Arabidopsis genome.
 second_blast = File.new("files/second_blast_unfiltered.txt", "w")
 second_blast.write("query\ttarget\te-value\tidentity\toverlap\tquery_length\tbit-score\tquery_seq\ttarget_seq\n")
 
-# We will only blast S. pombe sequences that are hits of the first blast
-spombe_fasta.each_entry do |fasta|  
-  if first_hits.values.include?(fasta.entry_id)
+# We will only blast S. pombe sequences that are best hits of the first blast
+spombe_fasta.each_entry do |spombe_seq|  
+  if first_hits.values.include?(spombe_seq.entry_id)
     
-    #puts fasta.entry_id
-
-    spombe_results = arabidopsis_factory.query(fasta)
+    spombe_results = arabidopsis_factory.query(spombe_seq)
 
     # Get the best hit from the BLAST results
     unless spombe_results.hits.empty?
@@ -117,7 +120,7 @@ spombe_fasta.each_entry do |fasta|
       # Print hits
       hit = spombe_results.hits.first
       puts
-      puts "#{fasta.entry_id} <==> #{hit.definition.split("|")[0]}"
+      puts "#{spombe_seq.entry_id} <==> #{hit.definition.split("|")[0]}"
       puts
       puts "Identity: #{hit.identity}\t e-value: #{hit.evalue}\t overlap: #{hit.identity}"
       puts
@@ -127,13 +130,21 @@ spombe_fasta.each_entry do |fasta|
       puts
     
       # Save hits in file
-      second_blast.write("#{fasta.entry_id}\t#{hit.definition.split("|")[0]}\t#{hit.evalue}\t#{hit.identity}\t#{hit.overlap}\t#{hit.query_len}\t#{hit.bit_score}\t#{hit.query_seq}\t#{hit.target_seq}\n")
+      second_blast.write("#{spombe_seq.entry_id}\t#{hit.definition.split("|")[0]}\t#{hit.evalue}\t#{hit.identity}\t#{hit.overlap}\t#{hit.query_len}\t#{hit.bit_score}\t#{hit.query_seq}\t#{hit.target_seq}\n")
     end
   end
 end
 
 second_blast.close()
 
+# We now retrieve the results of the second Blast into a new Hash
+second_hits = Hash.new
+
+File.readlines("files/first_blast_unfiltered.txt", chomp:true).each{ |hit|
+  unless hit =~ /query/ # Skip header
+    second_hits[hit.split("\t")[0]] = hit.split("\t")[1]
+  end
+}
 
 '''
 # BLAST the S. pombe sequence of the best hit against the Arabidopsis database
